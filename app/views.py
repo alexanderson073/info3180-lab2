b79caf46b0cf077067a55f8f60a912c23f94d483
Alex Anderson <alex.anderson02@mymona.uwi.edu>
2018-02-09 14:56:09 +0000

    Info3180-Lab2

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -23,6 +23,16 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    date_joined = format_date_joined()
+    return render_template('profile.html', formatted_date = date_joined)
+    
+    
+def format_date_joined():
+    date_joined = datetime.date(2018, 2, 7)
+    return "Joined " + date_joined.strftime("%B, %Y")
 
 
 ###
